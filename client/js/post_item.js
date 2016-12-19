d6de74ea87ecfe2bedee76425aea111a6c1af72a
lemeliner <pm.lemeliner@gmail.com>
2016-12-19 21:31:46 +0100

    mise en place de template

---
+++ client/js/post_item.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lemeliner on 19/12/2016.
+ */
+Template.postItem.helpers({
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
+})
